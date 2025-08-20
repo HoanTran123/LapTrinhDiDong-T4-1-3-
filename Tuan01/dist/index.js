34535313b2b45a29a78e2ae57643bd0fbbbcd83a
H82M19\Student <Student>
2025-08-20 08:31:57 +0700

    Comit-c1

---
+++ Tuan01/dist/index.js
@@ -0,0 +1,18 @@
+"use strict";
+// function hello(name: string): string {
+//   return `Hello, ${name}!`;
+// }
+// console.log(hello("200Lab"));
+// function person(name: string, age: number): string {
+//     return `Name and Age: ${name}, ${age}`;
+// }
+// console.log(person("John", 25));   
+// function student(name: string, age: number, grade: string): string {
+//     return `${person(name, age)}, Grade: ${grade}`;
+// }
+// console.log(student("John", 25, "A"));   
+// function Car (brand : string, year : number, model : string): string {
+//     return `brand and year and model : ${brand}, ${year} , ${model}`;
+// }
+// console.log(Car("vinfast", 2025, "XXX"))    ;   
+
---
+++ Tuan01/src/index.ts
@@ -0,0 +1,57 @@
+// function hello(name: string): string {
+//   return `Hello, ${name}!`;
+// }
+
+// console.log(hello("200Lab"));
+
+
+// function person(name: string, age: number): string {
+//     return `Name and Age: ${name}, ${age}`;
+// }
+// console.log(person("John", 25));   
+
+
+// function student(name: string, age: number, grade: string): string {
+//     return `${person(name, age)}, Grade: ${grade}`;
+// }
+// console.log(student("John", 25, "A"));   
+
+function Car (brand : string, year : number, model : string): string {
+    return `brand and year and model : ${brand}, ${year} , ${model}`;
+}
+console.log(Car("vinfast", 2025, "XXX"))    ;   
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  displayInfo(): void {
+    console.log(`Name: ${this.name}, Age: ${this.age}`);
+  }
+}
+
+const person = new Person("John", 25);
+person.displayInfo();
+
+
+class Student extends Person{
+    grade: string;
+    
+  constructor(name: string, age: number, grade :string) {
+    this.name = name;
+    this.age = age;
+    this.grade = grade;
+  }
+
+  displayInfo(): void {
+    console.log(`Name: ${this.name}, Age: ${this.age} , Grade:${this.grade}`);
+  }
+}
+
+const student = new Student("John", 25 , "A");
+person.displayInfo();
